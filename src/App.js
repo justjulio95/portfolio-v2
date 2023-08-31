dde93820d2af4b399f42dd053c1f9ab377bcdc06
Julio.S <just.julio95@gmail.com>
2023-08-31 10:08:46 -0400

    started the basis for the header component - played with style and general bootstrap tools

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Header from './components/header/header.component';
+
 function App() {
   return (
-    <div className='div-border container d-flex flex-column flex-sm-column flex-md-row'>
-      <h1 className='h1-border text-center'>Julio Suarez</h1>
-      <nav className='nav-border d-flex mx-3 justify-content-between'>
-        <h3 className='h3-border mx-3 mx-sm-3 mx-md-3'>Test</h3>
-        <h3 className='h3-border mx-3 mx-sm-3 mx-md-3'>Test</h3>
-        <h3 className='h3-border mx-3 mx-sm-3 mx-md-3'>Test</h3>
-      </nav>
-      <div className='div-border2 d-flex ms-2 justify-content-between'>
-        <img className='' src='' alt='test' />
-        <img src='' alt='test' />
-        <img src='' alt='test' />
-      </div>
-    </div>
+    <Header/>
+    
   );
 }
 
