86bcd69d6009da8f11042184a757569b5be4fccd
Julio.S <just.julio95@gmail.com>
2023-08-31 09:36:41 -0400

    playing with css designs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='div-border container d-flex flex-column flex-sm-column flex-md-row'>
+      <h1 className='h1-border text-center'>Julio Suarez</h1>
+      <nav className='nav-border d-flex mx-3 justify-content-between'>
+        <h3 className='h3-border mx-3 mx-sm-3 mx-md-3'>Test</h3>
+        <h3 className='h3-border mx-3 mx-sm-3 mx-md-3'>Test</h3>
+        <h3 className='h3-border mx-3 mx-sm-3 mx-md-3'>Test</h3>
+      </nav>
+      <div className='div-border2 d-flex ms-2 justify-content-between'>
+        <img className='' src='' alt='test' />
+        <img src='' alt='test' />
+        <img src='' alt='test' />
+      </div>
     </div>
   );
 }
