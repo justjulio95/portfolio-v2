546e8d4984f3c5b5d3d7c8eeadfb0a5fd1831208
Julio.S <just.julio95@gmail.com>
2023-09-01 15:45:22 -0400

    started on my projects section

---
+++ src/data/data.jsx
@@ -0,0 +1,44 @@
+import CrwnClothing from './images/crwn-clothing.png'
+
+export const projects = [
+  {
+    id: '1',
+    title: 'test',
+    tech: 'JavaScript',
+    description: 'this is a placeholder to see how this info behaves on this page',
+    image: CrwnClothing,
+    link: 'https://crwn-clothingjs.netlify.app/'
+  },
+  {
+    id: '2',
+    title: 'test',
+    tech: 'JavaScript',
+    description: 'this is a placeholder to see how this info behaves on this page',
+    image: CrwnClothing,
+    link: 'https://crwn-clothingjs.netlify.app/'
+  },
+  {
+    id: '3',
+    title: 'test',
+    tech: 'JavaScript',
+    description: 'this is a placeholder to see how this info behaves on this page',
+    image: CrwnClothing,
+    link: 'https://crwn-clothingjs.netlify.app/'
+  },
+  {
+    id: '4',
+    title: 'test',
+    tech: 'JavaScript',
+    description: 'this is a placeholder to see how this info behaves on this page',
+    image: CrwnClothing,
+    link: 'https://crwn-clothingjs.netlify.app/'
+  },
+  {
+    id: '5',
+    title: 'test',
+    tech: 'JavaScript',
+    description: 'this is a placeholder to see how this info behaves on this page',
+    image: CrwnClothing,
+    link: 'https://crwn-clothingjs.netlify.app/'
+  },
+]
